d1e7479b988171eff9082a2a7e2aa8867b4eb80e
77Fayy <Fay_3bdulrahman@hotmail.com>
2024-03-07 19:40:02 +0300

    Initial Commit

---
+++ Library/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Library
+//
+//  Created by fai alradhi on 26/08/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by fai alradhi on 26/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
